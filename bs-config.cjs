8a391417c06edfb0e4e692d1daaf6af58a4d69ec
Liks <82384131+WenLinkis@users.noreply.github.com>
2025-05-10 10:27:49 +0800

    chore: 调整 bs-config

--- bs-config.cjs
+++ bs-config.cjs
@@ -2,7 +2,8 @@ module.exports = {
   proxy: "http://localhost:58263",
   files: ["templates/"],
   injectChanges: false,
-  // open: false, // 是否自动打开浏览器
+  // open: "local", // 是否自动打开浏览器
+  browser: "vivaldi",
   // notify: false, // 是否显示通知
   // ghostMode: false, // 是否启用跨设备同步
   /* middleware: [
